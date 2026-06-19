66a04f250772ab7e68f5fc4e3f74dd7e842efbf4
xanarstic <literallyxanz@gmail.com>
2026-06-20 00:43:36 +0700

    connected database

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
 		'hostname' => 'localhost',
 		'username' => 'root',
 		'password' => '',
-		'database' => '',
+		'database' => 'R1',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
